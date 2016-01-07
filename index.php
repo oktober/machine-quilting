39b793a774141ac47431a0c6299fdcc5632521ab
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-07 12:58:33 -0700

    Add links, section dividers, and new customer discount info

--- index.php
+++ index.php
@@ -15,12 +15,35 @@
 
 <header>
     <img src="includes/images/logo_small.png" alt="Quilts 4 You Logo">
+    <h1>Quilts 4 You for your Long-Arm Machine Quilting services</h1>
 </header>
 
 <section>
-    <p>At Quilts 4 You - Heartspun Treasures, we are dedicated to providing you with the best long arm quilting service in a timely manner.</p>
+    <p>At Quilts 4 You, we are dedicated to providing you with the best long arm quilting service in a timely manner.</p>
     <p>Your quilt will always be unique and specially made just for you. We have been free motion quilting for XXXX years and are confident in our ability to deliver beautiful, custom quilts in a reasonable amount of time.</p>
     <p>We have many patterns to choose from and are continually adding to our selection. We look forward to collaborating with you to find a pattern that will enhance and showcase your beautiful quilt.</p>
+</section>
+
+<hr>
+
+<section>
+    <a href="">Pricing</a><br>
+    <a href="">Patterns</a><br>
+    <a href="">Thread &amp; Batting</a>
+</section>
+
+<hr>
+
+<section>
+    <a href="">Preparation Checklist</a><br>
+    <a href="">Order Information</a>
+</section>
+
+<hr>
+
+<section>
+    <h2>New Customer Discount</h2>
+    <p>Are you a new customer? Bring in your quilt(s) and mention our new customer discount to receive 10% off your machine quilting charge!</p>
 </section>
 
 </body>
