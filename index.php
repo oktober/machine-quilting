89d68e592cea9ce90a8c6bb37c69ff043f77f2ff
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-07 12:40:00 -0700

    Add Ubuntu font from Google

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Quilts 4 You | Long-Arm Machine Quilting</title>
     <!--<link rel="shortcut icon" href="includes/images/favicon.ico">-->
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="includes/css/styles.css">
 </head>
 
@@ -13,8 +14,13 @@
 <?php //include_once("includes/analyticstracking.php") ?>
 
 <header>
-    
+    <img src="includes/images/logo_small.png" alt="Quilts 4 You Logo">
 </header>
+
+<section>
+    <p>At Quilts 4 You - Heartspun Treasures, we are dedicated to providing you with the best long arm quilting service in a timely manner. We do not use pantograph patterns like many long arm machine quilters do so your quilt will always be unique and specially made just for you. We have many patterns to choose from and are continually adding to our selection. We will work with you to find a pattern that will complement and showcase your quilt.</p>
+    <p>In addition to long arm machine quilting we also offer a variety of services to meet your needs. Our design team would be glad to work closely with you to design and create a memorable quilt, from start to finish, for any occasion.</p>
+</section>
 
 </body>
 </html>
