e6c99c3a111e368792ef486672196f85f93bd109
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-07 12:29:06 -0700

    Add skeleton html code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quilts 4 You | Long-Arm Machine Quilting</title>
+    <!--<link rel="shortcut icon" href="includes/images/favicon.ico">-->
+    <link rel="stylesheet" href="includes/css/styles.css">
+</head>
+
+<body>
+<?php //include_once("includes/analyticstracking.php") ?>
+
+
+</body>
+</html>
