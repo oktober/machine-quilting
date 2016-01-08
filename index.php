cdfe84ff9e5589e5dae1c8bc0aa11eb891042bbc
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-08 09:50:15 -0700

    Reword sentence in body

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 <section>
     <p>At Quilts 4 You, we are dedicated to providing you with the best long arm quilting service in a timely manner.</p>
-    <p>Your quilt will always be unique and specially made just for you. We have been free motion quilting for XXXX years and are confident in our ability to deliver beautiful, custom quilts in a reasonable amount of time.</p>
+    <p>Your quilt will always be unique and specially made just for you. We have XXXX years of free motion quilting experience and are confident in our ability to deliver beautiful, custom quilts in a reasonable amount of time.</p>
     <p>We have many patterns to choose from and are continually adding to our selection. We look forward to collaborating with you to find a pattern that will enhance and showcase your beautiful quilt.</p>
 </section>
 
