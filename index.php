dbb42221cbeed9325e7d23ebb31d901cda749be4
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-13 10:42:19 -0700

    Add customer discount styles

--- index.php
+++ index.php
@@ -11,95 +11,97 @@
 </head>
 
 <body>
-<?php //include_once("includes/analyticstracking.php") ?>
+    <?php //include_once("includes/analyticstracking.php") ?>
 
-<header>
-    <div class="logo-container">
-        <img src="includes/images/logo_small_cropped.png" alt="Quilts 4 You Logo">
-    </div><!--
-    --><div class="tagline-container">
-        <h1>
-            <span class="company-name">Quilts 4 You</span><br>
-            for your<br>
-            Long-Arm Machine<br>
-            Quilting services
-        </h1>
-    </div>
-</header>
+    <header>
+        <div class="logo-container">
+            <img src="includes/images/logo_small_cropped.png" alt="Quilts 4 You Logo">
+        </div><!--
+        --><div class="tagline-container">
+            <h1>
+                <span class="company-name">Quilts 4 You</span><br>
+                for your<br>
+                Long-Arm Machine<br>
+                Quilting services
+            </h1>
+        </div>
+    </header>
 
-<hr class="divider">
+    <hr class="divider">
 
-<section class="info-group">
-    <p>At Quilts 4 You, we are dedicated to providing you with the best long arm quilting service in a timely manner.</p>
-    <p>Your quilt will always be unique and specially made just for you. We have XXXX years of free motion quilting experience and are confident in our ability to deliver beautiful, custom quilts in a reasonable amount of time.</p>
-    <p>We have many patterns to choose from and are continually adding to our selection. We look forward to collaborating with you to find a pattern that will enhance and showcase your beautiful quilt.</p>
-</section>
+    <section class="info-group">
+        <p>At Quilts 4 You, we are dedicated to providing you with the best long arm quilting service in a timely manner.</p>
+        <p>Your quilt will always be unique and specially made just for you. We have XXXX years of free motion quilting experience and are confident in our ability to deliver beautiful, custom quilts in a reasonable amount of time.</p>
+        <p>We have many patterns to choose from and are continually adding to our selection. We look forward to collaborating with you to find a pattern that will enhance and showcase your beautiful quilt.</p>
+    </section>
 
-<nav>
-    <h2 class="section-title">Get Started</h2>
-    <div class="info-group">
-        <div class="link"><a href="">Pricing >></a></div>
-        <div class="link"><a href="">Patterns >></a></div>
-        <div class="link"><a href="">Thread &amp; Batting >></a></div>
-        <div class="link"><a href="">Preparation Checklist >></a></div>
-        <div class="link"><a href="">Order Information >></a></div>
-    </div>
-</nav>
+    <nav>
+        <h2 class="section-title">Get Started</h2>
+        <div class="info-group">
+            <div class="link"><a href="">Pricing >></a></div>
+            <div class="link"><a href="">Patterns >></a></div>
+            <div class="link"><a href="">Thread &amp; Batting >></a></div>
+            <div class="link"><a href="">Preparation Checklist >></a></div>
+            <div class="link"><a href="">Order Information >></a></div>
+        </div>
+    </nav>
 
-<nav class="second-nav">
-    <div class="first-main-link"><a href="">Photo Gallery</a></div>
-    <p>Maybe add a teaser image here (that is also clickable)</p>
-    <div class="first-main-link"><a href="">About Quilts 4 You</a></div>
-</nav>
+    <nav class="second-nav">
+        <div class="main-link"><a href="">Photo Gallery</a></div>
+        <p>Maybe add a teaser image here (that is also clickable)</p>
+        <div class="main-link"><a href="">About Quilts 4 You</a></div>
+    </nav>
 
 
-<p>Create a Contact Us form here</p>
+    <hr>
 
-<hr>
+    <p>Create a Contact Us form here</p>
 
-<section>
-    <h2>New Customer Discount</h2>
-    <p>Are you a new customer? Bring in your quilt(s) and mention our new customer discount to receive 10% off your machine quilting charge!</p>
-</section>
+    <hr>
 
-<hr>
+    <section>
+        <h2 class="section-title">New Customer Discount</h2>
+        <p class="info-group">Are you a new customer? Bring in your quilt(s) and mention our new customer discount to receive <span class="noteworthy">10% off your machine quilting charge!</span></p>
+    </section>
 
-<section>
-    <h2>Hear from our satisfied customers</h2>
+    <hr>
 
-    <q>I received my quilt on Saturday and it is beautiful, you did a great job! I have already put the binding on and it is ready to mail to the bride and groom... Again, thanks for the beautiful quilt and I look forward working with you in the future.</q>
-    <cite>Jeanice S. in North Carolina</cite>
+    <section>
+        <h2>Hear from our satisfied customers</h2>
 
-    <br><br>
-    <q>I was very pleased with your work. You helped me very much with my first big project. I would certainly recommend you to others! Thank you for all your help!</q>
-    <cite>Linda K. in Utah</cite>
+        <q>I received my quilt on Saturday and it is beautiful, you did a great job! I have already put the binding on and it is ready to mail to the bride and groom... Again, thanks for the beautiful quilt and I look forward working with you in the future.</q>
+        <cite>Jeanice S. in North Carolina</cite>
 
-    <br><br>
-    <q>As always, Ann does an impeccable job with both design and technique. The quilt required a lot of time and effort from me to piece and I appreciate her taking equal time and effort to help in creating a treasured keepsake. Thanks Ann!</q>
-    <cite>Sherida P. in Utah</cite><br>
-    <a>Add image here with a popup showing the quilt (kaleidoscopequilt.jpg)</a>
+        <br><br>
+        <q>I was very pleased with your work. You helped me very much with my first big project. I would certainly recommend you to others! Thank you for all your help!</q>
+        <cite>Linda K. in Utah</cite>
 
-    <br><br>
-    <q>I am so pleased! It is absolutely beautiful! You did just exactly what I wanted, and I can't thank you enough!</q>
-    <cite>JS in Nevada</cite>
+        <br><br>
+        <q>As always, Ann does an impeccable job with both design and technique. The quilt required a lot of time and effort from me to piece and I appreciate her taking equal time and effort to help in creating a treasured keepsake. Thanks Ann!</q>
+        <cite>Sherida P. in Utah</cite><br>
+        <a>Add image here with a popup showing the quilt (kaleidoscopequilt.jpg)</a>
 
-    <br><br>
-    <q>Thank you soooooooo much. It is beautiful and I can not wait for my husband to come home and see it on the bed. You really did a beautiful job. I have a label and I am excited to affix it on the back. I am the proud owner of a quilt that Ann Morgan Quilted!!!!! It is wonderful. I really love the designs. Can't wait to show it off at work tomorrow!!!</q>
-    <cite>Caryl J. in Utah</cite><br>
-    <a>Add image here with a popup showing the quilt (sunflower_quilt.jpg)</a>
-</section>
+        <br><br>
+        <q>I am so pleased! It is absolutely beautiful! You did just exactly what I wanted, and I can't thank you enough!</q>
+        <cite>JS in Nevada</cite>
 
-<footer>
-    <address>
-        Quilts 4 You<br>
-        <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow">647 E Bristlecone Lane<br>
-        PO Box 21<br>
-        Delta, UT 84624</a>
-    </address>
-    <p>Your destination for machine quilting, quilt binding, and quilting supplies for Utah &amp; the continental US</p>
-    <p>&copy;2016 Quilts 4 You</p>
-    <p>Designed &amp; Built by: Techie Grit</p>
-</footer>
+        <br><br>
+        <q>Thank you soooooooo much. It is beautiful and I can not wait for my husband to come home and see it on the bed. You really did a beautiful job. I have a label and I am excited to affix it on the back. I am the proud owner of a quilt that Ann Morgan Quilted!!!!! It is wonderful. I really love the designs. Can't wait to show it off at work tomorrow!!!</q>
+        <cite>Caryl J. in Utah</cite><br>
+        <a>Add image here with a popup showing the quilt (sunflower_quilt.jpg)</a>
+    </section>
+
+    <footer>
+        <address>
+            Quilts 4 You<br>
+            <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow">647 E Bristlecone Lane<br>
+            PO Box 21<br>
+            Delta, UT 84624</a>
+        </address>
+        <p>Your destination for machine quilting, quilt binding, and quilting supplies for Utah &amp; the continental US</p>
+        <p>&copy;2016 Quilts 4 You</p>
+        <p>Designed &amp; Built by: Techie Grit</p>
+    </footer>
 
 </body>
 </html>
