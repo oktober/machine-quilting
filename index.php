74939dc3b2d04b9590feacf876b3f27a043d27c8
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-13 11:45:20 -0700

    Add styles to footer and add new serif font for contrast

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Quilts 4 You | Long-Arm Machine Quilting</title>
     <!--<link rel="shortcut icon" href="includes/images/favicon.ico">-->
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Volkhov:400,700italic,400italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="includes/css/styles.css">
 </head>
 
@@ -38,11 +39,11 @@
     <nav>
         <h2 class="section-title">Get Started</h2>
         <div class="info-group">
-            <div class="link"><a href="">Pricing &gt;&gt;</a></div>
-            <div class="link"><a href="">Patterns &gt;&gt;</a></div>
-            <div class="link"><a href="">Thread &amp; Batting &gt;&gt;</a></div>
-            <div class="link"><a href="">Preparation Checklist &gt;&gt;</a></div>
-            <div class="link"><a href="">Order Information &gt;&gt;</a></div>
+            <div class="link"><a href="page.php">Pricing &gt;&gt;</a></div>
+            <div class="link"><a href="page.php">Patterns &gt;&gt;</a></div>
+            <div class="link"><a href="page.php">Thread &amp; Batting &gt;&gt;</a></div>
+            <div class="link"><a href="page.php">Preparation Checklist &gt;&gt;</a></div>
+            <div class="link"><a href="page.php">Order Information &gt;&gt;</a></div>
         </div>
     </nav>
 
@@ -71,41 +72,47 @@
 
         <div class="info-group">
             <q>I received my quilt on Saturday and it is beautiful, you did a great job! I have already put the binding on and it is ready to mail to the bride and groom... Again, thanks for the beautiful quilt and I look forward working with you in the future.</q>
-            <cite>Jeanice S. in North Carolina</cite>
+            <cite>Jeanice S. from NC</cite>
 
             <br>
 
             <q>I was very pleased with your work. You helped me very much with my first big project. I would certainly recommend you to others! Thank you for all your help!</q>
-            <cite>Linda K. in Utah</cite>
+            <cite>Linda K. from UT</cite>
 
             <br>
             <q>As always, Ann does an impeccable job with both design and technique. The quilt required a lot of time and effort from me to piece and I appreciate her taking equal time and effort to help in creating a treasured keepsake. Thanks Ann!</q>
-            <cite>Sherida P. in Utah</cite>
+            <cite>Sherida P. from UT</cite>
             <p class="image-popup"><a href="includes/images/customer_review_quilts/kaleidoscopequilt-xs.jpeg">TODO: View the quilt</a></p>
 
             <br>
             <q>I am so pleased! It is absolutely beautiful! You did just exactly what I wanted, and I can't thank you enough!</q>
-            <cite>JS in Nevada</cite>
+            <cite>JS from NV</cite>
 
             <br>
             <q>Thank you soooooooo much. It is beautiful and I can not wait for my husband to come home and see it on the bed. You really did a beautiful job. I have a label and I am excited to affix it on the back. I am the proud owner of a quilt that Ann Morgan Quilted!!!!! It is wonderful. I really love the designs. Can't wait to show it off at work tomorrow!!!</q>
-            <cite>Caryl J. in Utah</cite>
+            <cite>Caryl J. from UT</cite>
             <p class="image-popup"><a href="includes/images/customer_review_quilts/sunflower_quilt-xs.jpeg">TODO: View the quilt</a></p>
         </div>
     </section>
 
-    <hr>
-
     <footer>
+        <p>Your destination for machine quilting and quilting supplies for Utah &amp; the continental US</p>
+
+        <section class="copyright">
+            &copy;2016 Quilts 4 You
+            <br><br>
+            <span class="credit">Designed &amp; Built by: Techie Grit</span>
+        </section>
+
         <address>
             Quilts 4 You<br>
-            <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow">647 E Bristlecone Lane<br>
-            PO Box 21<br>
-            Delta, UT 84624</a>
+            <div class="address-link">
+                <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow">647 E Bristlecone Lane<br>
+                    PO Box 21<br>
+                    Delta, UT 84624<br>
+                    (Click for map)</a>
+            </div>
         </address>
-        <p>Your destination for machine quilting, quilt binding, and quilting supplies for Utah &amp; the continental US</p>
-        <p>&copy;2016 Quilts 4 You</p>
-        <p>Designed &amp; Built by: Techie Grit</p>
     </footer>
 
 </body>
