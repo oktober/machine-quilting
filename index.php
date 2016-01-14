41e9ad692696190fef12a5d65dcec77d2bf77416
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-14 10:42:14 -0700

    Reorder CSS to be more readable and to group things together

--- index.php
+++ index.php
@@ -16,8 +16,9 @@
 
     <header>
         <div class="logo-container">
-            <img src="includes/images/logo_small_cropped.png" alt="Quilts 4 You Logo">
-        </div><div class="tagline-container">
+            <img src="includes/images/logo_small_cropped.png" alt="Quilts 4 You Logo" class="logo">
+        </div>
+        <div class="tagline-container">
             <h1>
                 <span class="company-name">Quilts 4 You</span><br>
                 for your<br>
@@ -63,7 +64,6 @@
 
     <section>
         <h2 class="section-title">Customer Reviews</h2>
-
         <div class="info-group">
             <q>I received my quilt on Saturday and it is beautiful, you did a great job! I have already put the binding on and it is ready to mail to the bride and groom... Again, thanks for the beautiful quilt and I look forward working with you in the future.</q>
             <cite>Jeanice S. from NC</cite>
@@ -86,10 +86,12 @@
             </div>
 
             <br>
+
             <q>I am so pleased! It is absolutely beautiful! You did just exactly what I wanted, and I can't thank you enough!</q>
             <cite>JS from NV</cite>
 
             <br>
+
             <div id="sunflower-review"></div>
             <q>Thank you soooooooo much. It is beautiful and I can not wait for my husband to come home and see it on the bed. You really did a beautiful job. I have a label and I am excited to affix it on the back. I am the proud owner of a quilt that Ann Morgan Quilted!!!!! It is wonderful. I really love the designs. Can't wait to show it off at work tomorrow!!!</q>
             <cite>Caryl J. from UT</cite>
@@ -104,8 +106,6 @@
     </section>
 
     <footer>
-
-
         <section class="footer-info">
             <p class="summary">
                 Your destination for machine quilting and quilting supplies for Utah &amp; the continental US
