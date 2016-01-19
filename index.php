cdadc67ce7481738e93f7a3eea4e9356ffe8a0cf
Stacie Farmer <farmerwebdesign@gmail.com>
2016-01-19 12:21:40 -0700

    Add additional info to make site more accessible

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html dir="LTR" lang="en-US">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Quilts 4 You is your destination for long arm machine quilting and quilting supplies for Utah and the continental US. We are dedicated to providing you with the best long-arm quilting service in a timely manner with a quilt that is unique and made just for you.">
     <title>Quilts 4 You | Long-Arm Machine Quilting</title>
     <!--<link rel="shortcut icon" href="includes/images/favicon.ico">-->
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
@@ -39,18 +40,18 @@
     <nav>
         <h2 class="section-title">Get Started</h2>
         <div class="info-group">
-            <div class="link"><a href="page.php">Pricing &gt;&gt;</a></div>
-            <div class="link"><a href="page.php">Patterns &gt;&gt;</a></div>
-            <div class="link"><a href="page.php">Thread &amp; Batting &gt;&gt;</a></div>
-            <div class="link"><a href="page.php">Preparation Checklist &gt;&gt;</a></div>
-            <div class="link"><a href="page.php">Order Information &gt;&gt;</a></div>
+            <div class="link"><a href="page.php" title="Link to Pricing page">Pricing &gt;&gt;</a></div>
+            <div class="link"><a href="page.php" title="Link to Patterns page">Patterns &gt;&gt;</a></div>
+            <div class="link"><a href="page.php" title="Link to Thread and Batting page">Thread &amp; Batting &gt;&gt;</a></div>
+            <div class="link"><a href="page.php" title="Link to Preparation Checklist">Preparation Checklist &gt;&gt;</a></div>
+            <div class="link"><a href="page.php" title="Link to Order Information">Order Information &gt;&gt;</a></div>
         </div>
     </nav>
 
     <nav class="second-nav">
-        <div class="main-link"><a href="">Photo Gallery &gt;&gt;</a></div>
-        <div class="main-link"><a href="">About Quilts 4 You &gt;&gt;</a></div>
-        <div class="main-link"><a href="">Quilt Shop &gt;&gt;</a></div>
+        <div class="main-link"><a href="" title="Link to Photo Gallery">Photo Gallery &gt;&gt;</a></div>
+        <div class="main-link"><a href="" title="Link to About Quilts 4 You">About Quilts 4 You &gt;&gt;</a></div>
+        <div class="main-link"><a href="" title="Link to Quilt Shop">Quilt Shop &gt;&gt;</a></div>
     </nav>
 
     <section>
@@ -78,11 +79,11 @@
             <div id="kaleidoscope-review"></div>
             <q>As always, Ann does an impeccable job with both design and technique. The quilt required a lot of time and effort from me to piece and I appreciate her taking equal time and effort to help in creating a treasured keepsake. Thanks Ann!</q>
             <cite>Sherida P. from UT</cite>
-            <p class="image-popup"><a href="#kaleidoscope">View the finished quilt</a></p>
+            <p class="image-popup"><a href="#kaleidoscope" title="Click to view Sherida's finished quilt">View the finished quilt</a></p>
             <div class="lightbox" id="kaleidoscope">
                 <figure>
-                    <a href="#kaleidoscope-review" class="closemsg"></a>
-                    <img src="includes/images/customer_review_quilts/kaleidoscopequilt-xs.jpeg" alt="Picture of kaleidoscope quilt quilted by Ann Morgan">
+                    <a href="#kaleidoscope-review" class="closemsg" title="Click to close image"></a>
+                    <img src="includes/images/customer_review_quilts/kaleidoscopequilt-xs.jpeg" alt="Picture of finished kaleidoscope quilt with quilting by Ann Morgan">
                 </figure>
             </div>
 
@@ -96,11 +97,11 @@
             <div id="sunflower-review"></div>
             <q>Thank you soooooooo much. It is beautiful and I can not wait for my husband to come home and see it on the bed. You really did a beautiful job. I have a label and I am excited to affix it on the back. I am the proud owner of a quilt that Ann Morgan Quilted!!!!! It is wonderful. I really love the designs. Can't wait to show it off at work tomorrow!!!</q>
             <cite>Caryl J. from UT</cite>
-            <p class="image-popup"><a href="#sunflower">View the finished quilt</a></p>
+            <p class="image-popup"><a href="#sunflower" title="Click to view Caryl's finished quilt">View the finished quilt</a></p>
             <div class="lightbox" id="sunflower">
                 <figure>
-                    <a href="#sunflower-review" class="closemsg"></a>
-                    <img src="includes/images/customer_review_quilts/sunflower_quilt-xs.jpeg" alt="Picture of sunflower quilt quilted by Ann Morgan">
+                    <a href="#sunflower-review" class="closemsg" title="Click to close image"></a>
+                    <img src="includes/images/customer_review_quilts/sunflower_quilt-xs.jpeg" alt="Picture of finished sunflower quilt with quilting by Ann Morgan">
                 </figure>
             </div>
         </div>
@@ -120,7 +121,7 @@
         --><address>
             Quilts 4 You<br>
             <div class="address-link">
-                <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow">647 E Bristlecone Lane<br>
+                <a href="https://www.google.com/maps/place/647+Bristlecone+Ln,+Delta,+UT+84624/@39.351987,-112.561154,15z/data=!4m2!3m1!1s0x874cb587c6ee9dc9:0xfb0bb9aa2960a30d?hl=en" target="_blank" rel="nofollow" title="Click to view Google Map of our location">647 E Bristlecone Lane<br>
                     PO Box 21<br>
                     Delta, UT 84624<br>
                     (Click for map)</a>
