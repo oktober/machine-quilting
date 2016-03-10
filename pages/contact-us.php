739428f2aa3f7a6ca0827e6d47d471973d9dc2a2
Stacie Farmer <farmerwebdesign@gmail.com>
2016-03-10 12:43:46 -0700

    Add basic ajax to handle emailing the contact form

--- pages/contact-us.php
+++ pages/contact-us.php
@@ -1,5 +1,11 @@
 <?php
 require_once('../classes/Email.php');
 
-echo sendEmail($_POST['name-field'], $_POST['email-field'], $_POST['message-field']);
-header('Location: http://quilts4you.com?q=1');
+//check if mname field is empty - otherwise it's a bot & don't send
+
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    return sendEmail($_POST['name-field'], $_POST['email-field'], $_POST['message-field']);
+} else {
+    echo sendEmail($_POST['name-field'], $_POST['email-field'], $_POST['message-field']);
+    header('Location: http://quilts4you.com?q=1');
+}
