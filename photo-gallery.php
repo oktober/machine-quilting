6c1f76e66ce99021d34f8e361c2bc194dc062ff1
Stacie Farmer <farmerwebdesign@gmail.com>
2016-04-13 10:39:14 -0600

    Add images to gallery page with captions and popups

--- photo-gallery.php
+++ photo-gallery.php
@@ -10,7 +10,139 @@
     <section>
         <h2 class="section-title page-title">Photo Gallery</h2>
         <div class="page-detail">
-            <p class="comment">Click on an image to expand</p>
+            <p class="comment bold">Click on image to expand</p>
+            <div>
+                <div id="hourglass-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#hourglass" title="Click to view hourglass quilt">
+                            <img src="includes/images/photo-gallery/hourglassquilt.jpg" class="gallery-img" alt="Picture of finished hourlgass quilt with quilting by Ann Morgan">
+                    </a></p>
+                    <figcaption>Hour Glass Quilt <span class="comment">(Daisy &amp; Vine Curl Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="hourglass">
+                    <figure>
+                        <a href="#hourglass-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/hourglassquilt.jpg" alt="Picture of finished hourlgass quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="bunny-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#bunny" title="Click to view pink bunny quilt">
+                            <img src="includes/images/photo-gallery/pinkbunnyquilt.jpg" class="gallery-img" alt="Picture of finished pink bunny quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Pink Bunny Quilt <span class="comment">(Happy Flower, Loops, &amp; Daisy Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="bunny">
+                    <figure>
+                        <a href="#bunny-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/pinkbunnyquilt.jpg" alt="Picture of finished pink bunny quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="xmas-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#xmas" title="Click to view christmas quilt">
+                            <img src="includes/images/photo-gallery/christmasquilt.jpg" class="gallery-img" alt="Picture of finished christmas quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Christmas Quilt <span class="comment">(Custom Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="xmas">
+                    <figure>
+                        <a href="#xmas-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/christmasquilt.jpg" alt="Picture of finished christmas quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="bluestar-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#bluestar" title="Click to view blue star quilt">
+                            <img src="includes/images/photo-gallery/bluestarquilt.jpg" class="gallery-img" alt="Picture of finished blue star quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Blue Star Quilt <span class="comment">(Stars &amp; Stipple Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="bluestar">
+                    <figure>
+                        <a href="#bluestar-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/bluestarquilt.jpg" alt="Picture of finished blue star quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="kaleidoscope-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#kaleidoscope" title="Click to view kaleidoscope quilt">
+                            <img src="includes/images/photo-gallery/kaleidoscopequilt.jpg" class="gallery-img" alt="Picture of finished kaleidoscope quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Kaleidoscope Quilt <span class="comment">(Custom Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="kaleidoscope">
+                    <figure>
+                        <a href="#kaleidoscope-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/kaleidoscopequilt.jpg" alt="Picture of finished kaleidoscope quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="animal-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#animal" title="Click to view baby animal quilt">
+                            <img src="includes/images/photo-gallery/babyanimalquilt.jpg" class="gallery-img" alt="Picture of finished baby animal quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Baby Animal Quilt <span class="comment">(Stipple, Loops, &amp; Swirls Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="animal">
+                    <figure>
+                        <a href="#animal-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/babyanimalquilt.jpg" alt="Picture of baby animal quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="sampler-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#sampler" title="Click to view New York sampler quilt">
+                            <img src="includes/images/photo-gallery/ny_sampler.jpg" class="gallery-img" alt="Picture of finished New York sampler quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>New York Sampler Quilt <span class="comment">(Stipple, Daisy, &amp; Basic Leaf Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="sampler">
+                    <figure>
+                        <a href="#sampler-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/ny_sampler.jpg" alt="Picture of New York sampler quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="redwork-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#redwork" title="Click to view redwork runner quilt">
+                            <img src="includes/images/photo-gallery/redworkrunner.jpg" class="gallery-img" alt="Picture of finished redwork runner quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>
+                        Redwork Runner <span class="comment">(Custom Quilting)</span>
+                        <br><a href="" target="_blank">Original Pattern available for sale</a>
+                    </figcaption>
+                </figure>
+                <div class="lightbox" id="redwork">
+                    <figure>
+                        <a href="#redwork-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/redworkrunner.jpg" alt="Picture of redwork runner quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <div id="sunflower-img"></div>
+                <figure>
+                    <p class="image-popup"><a href="#sunflower" title="Click to view sunflower runner quilt">
+                            <img src="includes/images/photo-gallery/sunflower_quilt.jpg" class="gallery-img" alt="Picture of finished sunflower runner quilt with quilting by Ann Morgan">
+                        </a></p>
+                    <figcaption>Sunflower Quilt <span class="comment">(Daisy Flowers &amp; Vine Curl Quilting)</span></figcaption>
+                </figure>
+                <div class="lightbox" id="sunflower">
+                    <figure>
+                        <a href="#sunflower-img" class="closemsg" title="Click to close image"></a>
+                        <img src="includes/images/photo-gallery/sunflower_quilt.jpg" alt="Picture of sunflower runner quilt with quilting by Ann Morgan">
+                    </figure>
+                </div>
+
+                <p><a href="#" class="return-top">Return to top</a></p>
+            </div>
         </div>
     </section>
 
