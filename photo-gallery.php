252a2cd7cc6174fc3c8b3b0e4c9ee6805ceb22cd
Stacie Farmer <farmerwebdesign@gmail.com>
2016-04-13 10:01:09 -0600

    Create skeleton with base styles for Photo Gallery page

---
+++ photo-gallery.php
@@ -0,0 +1,17 @@
+<?php require_once("includes/header.php"); ?>
+
+
+    <nav class="primary-nav">
+        <a href="index.php" title="Link to Home Page">Get Started <span class="italicize">(Home)</span> &gt;&gt;</a>
+        <a href="" title="Link to About Quilts 4 You">About Quilts 4 You &gt;&gt;</a>
+        <a href="" title="Link to Quilt Shop">Quilt Shop &gt;&gt;</a>
+    </nav>
+
+    <section>
+        <h2 class="section-title page-title">Photo Gallery</h2>
+        <div class="page-detail">
+            <p class="comment">Click on an image to expand</p>
+        </div>
+    </section>
+
+<?php require_once("includes/footer.php"); ?>
