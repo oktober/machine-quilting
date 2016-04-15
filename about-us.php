9ba856a55b3adc7f18bce20cba161cbab3c71bed
Stacie Farmer <farmerwebdesign@gmail.com>
2016-04-15 08:11:13 -0600

    Add info and picture to About Us page

--- about-us.php
+++ about-us.php
@@ -10,6 +10,15 @@
     <section>
         <h2 class="section-title page-title">About Quilts 4 You</h2>
         <div class="page-detail">
+            <img src="includes/images/quilter_ann.jpg" class="profile-img" alt="Picture of E. Ann Morgan standing in front of a quilt she created">
+            <h3 class="profile-title">E. Ann Morgan <span class="comment">(Owner)</span></h3>
+            <p>Sewing has been my life since my teenage years when my mother taught me to sew. Later, these skills proved most beneficial as I was able to contribute to our family of five children, by sewing clothes for them.</p>
+            <p>In January of 1991, my sewing world took on a wonderful, new dimension when I discovered "quilting". Piecing, quilting and applique offered me an outlet to express my designing creativity.</p>
+            <p>Through the years I have found myself teaching at various venues, such as the Utah Quilt Guild Quilt Festival, Bryce Canyon Retreat, The Snow Goose Quilt Show Festival and numerous guilds.</p>
+            <p>In May of 2000, Quilts 4 You received our Gammill quilting machine. It has brought us many years of enjoyment and personal satisfaction. It is exciting to stretch our creativity with designing new quilting patterns. </p>
+            <p>When it comes to machine quilting, we are always keeping an eye out for new techniques and quilting patterns. We regularly attend quilting classes at the Home Machine Quilters Showcase (HMQS) in Salt Lake City and we also take classes from several international long arm instructors each year. The knowledge we gain from these classes is priceless; and our quilting patterns show it. </p>
+            <p>Besides machine quilting, I also get a lot of enjoyment in designing quilt patterns and projects. You can find a few of my original patterns in our <a href="http://shop.quilts4you.com" target="_blank" class="noteworthy">on-line quilt shop</a>. </p>
+            <p>Sharing the art of quiltmaking is the joy of my life!</p>
         </div>
     </section>
 
